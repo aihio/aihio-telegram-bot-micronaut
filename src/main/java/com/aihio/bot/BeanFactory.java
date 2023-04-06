50ac4840dc2ea214fca5cbb079d9819a1beb0e66
aihio <daniil.nikishyn@gmail.com>
2023-04-06 03:09:23 +0300

    added BeanFactory. removed BotRegistrationService

--- src/main/java/com/aihio/bot/BeanFactory.java
+++ src/main/java/com/aihio/bot/BeanFactory.java
@@ -1,9 +1,8 @@
 package com.aihio.bot;
 
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Property;
-import io.micronaut.context.event.ApplicationEventListener;
-import io.micronaut.discovery.event.ServiceReadyEvent;
-import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -11,28 +10,33 @@ import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Singleton
-public class BotRegistrationService implements ApplicationEventListener<ServiceReadyEvent> {
-    private static final Logger logger = LoggerFactory.getLogger(BotRegistrationService.class.getName());
+@Factory
+public class BeanFactory {
+    private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class.getName());
 
     @Property(name = "bot.name")
     private String botName;
-
     @Property(name = "bot.token")
     private String botToken;
     @Property(name = "bot.path")
     private String botPath;
 
-    @Override
-    public void onApplicationEvent(ServiceReadyEvent event) {
-        try {
-            logger.info("Registering webhook bot");
-            var telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+    @Bean
+    public SetWebhook setWebhookInstance() {
+        return SetWebhook.builder().url(botPath).build();
+    }
 
-            telegramBotsApi.registerBot(new WebHookBot(botToken, botName, botPath),
-                    SetWebhook.builder().url(botPath).build());
+    @Bean
+    public WebHookBot springWebhookBot(SetWebhook setWebhook) {
+        logger.info("Registering webhook bot");
+        var bot = new WebHookBot(botToken, botName, botPath);
+        try {
+            var telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            bot.setWebhook(setWebhook);
+            telegramBotsApi.registerBot(bot,setWebhook);
         } catch (TelegramApiException e) {
             logger.error("Error registering bot", e);
         }
+        return bot;
     }
 }
